b7b099929490d06d339cb0e7920df21626b6a52c
“Hines” <Hine12138@gmail.com>
2025-08-26 13:51:54 +0800

    rewr

--- app/service/ave.js
+++ app/service/ave.js
@@ -142,7 +142,7 @@ class Ave extends Service {
     const create_day = groupSort.create_day || 10;
     const create_min =
       Math.round(new Date().getTime() / 1000) - create_day * 24 * 3600;
-    const holder_min = 50;
+    const holder_min = 10;
     const uri = `${AVE_API_URL}v1api/v4/tokens/treasure/list?chain=solana&sort=${sort_field}&sort_dir=${sort_order}&created_at_min=${create_min}&marketcap_min=${mcp_min}&marketcap_max=${mcp_max}&holder_min=${holder_min}&pageNO=1&pageSize=500&category=${category}`;
 
     const X_AUTH = await this.getXAuth();
